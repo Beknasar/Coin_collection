e0087f6db652ba7cf0be74417b5af96879d49fec
Beknasar <680633@gmail.com>
2021-05-03 17:59:32 +0600

    Созданы модели данных стран, материалов, валют. Модели были зарегистрированы в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from webapp.models import Country, Currency, Material
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+    search_fields = ('name__icontains',)
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(Material)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 from django.db import models
+from django.utils import timezone
+from datetime import date
 
-# Create your models here.
+
+class Material(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='название материала')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "материал"
+        verbose_name_plural = "материалы"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="название страны")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "страна"
+        verbose_name_plural = "страны"
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="название страны")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "валюта"
+        verbose_name_plural = "валюты"
