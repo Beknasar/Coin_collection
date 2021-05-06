692ec3f448d192c966dd5d893ada6cecd4996a79
Beknasar <680633@gmail.com>
2021-05-06 19:33:32 +0600

    Обновлена вёрстка базовой страницы, добавлено поиск по году и стране монет.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 class IndexView(ListView):
     template_name = 'coins/index.html'
     context_object_name = 'coins'
-    paginate_by = 3
+    paginate_by = 4
     paginate_orphans = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -25,7 +25,11 @@ class IndexView(ListView):
         form = SearchForm(data=self.request.GET)
         if form.is_valid():
             search = form.cleaned_data['search']
+            print(search)
+            print(data)
             if search:
-                data = data.filter(Q(nominal__icontains=search) | Q(country__icontains=search))
+                data = data.filter(Q(country__name__icontains=search) | Q(year_of_issue__icontains=search))
 
         return data.order_by('-country')
+
+
